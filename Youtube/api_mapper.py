e2d005165d1a5701de9cbea291466e421a7ddc1e
professor91 <keshavcode3@gmail.com>
2023-01-31 21:31:23 +0530

    added parser_functions for all endpoints

--- Youtube/api_mapper.py
+++ Youtube/api_mapper.py
@@ -35,6 +35,7 @@ API_MAPPER = {
             "id": None,
             "part": "snippet, contentDetails",
         },
+        "parse_function": "parse_channel_section_list_response",
     },
 
     "playlists": {
@@ -54,6 +55,7 @@ API_MAPPER = {
             "part": "snippet, contentDetails",
             "maxResults": 10,
         },
+        "parse_function": "parse_playlist_list_response",
     },
 
     "playlist_videos": {
@@ -73,6 +75,7 @@ API_MAPPER = {
             "part": "snippet, contentDetails",
             "maxResults": 10
         },
+        "parse_function": "parse_playlist_video_list_response",
     },
 
     "video_by_id": {
@@ -81,6 +84,7 @@ API_MAPPER = {
             "id": None,
             "part": "contentDetails, id, localizations, snippet, statistics, status, topicDetails",
         },
+        "parse_function": "parse_video_response",
     },
 
 
@@ -92,6 +96,7 @@ API_MAPPER = {
             "order": "time",
             "textFormat": "plainText",
         },
+        "parse_function": "parse_comments_list_response",
     },
     "replies_to_comment": {
         "endpoint": "/comments",
@@ -100,7 +105,8 @@ API_MAPPER = {
             "part": "snippet",
             "maxResults": 20,
             "textFormat": "plainText",
-        }
+        },
+        "parse_function": "parse_comments_list_response",
     },
     "comment_by_id": {
         "endpoint": "/comments",
@@ -108,7 +114,8 @@ API_MAPPER = {
             "id": None,
             "part": "snippet",
             "textFormat": "plainText",
-        }
+        },
+        "parse_function": "parse_comment_responses",
     },
 
     "caption": {
